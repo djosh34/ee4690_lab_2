2e07890716c82b1f0c05b2ab80cdb011e13659dd
Joshua Azimullah <j.r.azimullah@student.tudelft.nl>
2024-06-06 05:59:08 +0200

    tcl script ready

--- Genus_Lab2/run_script.tcl
+++ Genus_Lab2/run_script.tcl
@@ -4,15 +4,36 @@ set_attribute lib_search_path "./libs"
 
 set_attribute information_level 7
 
-set_attribute hdl_search_path "./rtl/matrix_multiplier"  
-
 set_attribute library [glob -directory [get_attribute lib_search_path] -tails *.lib]
 
-set mylist [glob -directory [get_attribute hdl_search_path] -tails matrix_multiplier.vhd]
+# set_attribute hdl_search_path "./rtl/matrix_multiplier"  
 
-read_hdl -vhdl $mylist
+# set mylist [glob -directory [get_attribute hdl_search_path] -tails matrix_multiplier.vhd]
 
-elaborate matrix_multiplier
+# read_hdl -vhdl $mylist
+
+# Specify the VHDL version
+# set_attribute hdl_language vhdl-2008
+set_db / .hdl_vhdl_read_version 2008
+
+
+# List of VHDL files to be read in the specified order
+# set mylist [list \
+#     "./rtl/predict/predict_package.vhd" \
+#     "./rtl/matrix_2_output/matrix_2_output.vhd" \
+#     "./rtl/xnor_popcount/xnor_popcount.vhd" \
+#     "./rtl/predict/predict.vhd"]
+
+set mylist [glob -directory "./rtl" -tails "./predict/predict_package.vhd ./matrix_2_output/matrix_2_output.vhd ./xnor_popcount/xnor_popcount.vhd" "./predict/predict.vhd"]
+
+# Read HDL files in specified order
+foreach file $mylist {
+    read_hdl -vhdl2008 $file
+}
+
+elaborate matrix_2_output 
+# elaborate xnor_popcount 
+# elaborate predict 
 
 report hierarchy > ./output/hierarchy.txt
 
