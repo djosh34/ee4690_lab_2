43214c449cba066a511eac472b619277cdaf7e2a
Joshua Azimullah <j.r.azimullah@student.tudelft.nl>
2024-06-07 14:26:59 +0200

    low effort

--- Genus_Lab2/run_script.tcl
+++ Genus_Lab2/run_script.tcl
@@ -55,7 +55,8 @@ check_design -unresolved > ./output/check_design.txt
 read_sdc ./timing.sdc
 
 #---- Perform synthesis ------------------#
-set_attribute syn_global_effort high 
+# set_attribute syn_global_effort high 
+set_attribute syn_global_effort none 
 # technology independent RTL optimiztion e.g. dead code removal
 syn_gen
 # map generic gates to gates from the chosen target technology library
