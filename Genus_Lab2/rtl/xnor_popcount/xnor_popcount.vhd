f64b5a6b9483c5506c3615b3fc04de634bcbeece
Joshua Azimullah <j.r.azimullah@student.tudelft.nl>
2024-06-07 10:27:39 +0200

    Lab2 Fri Jun  7 10:27:39 CEST 2024

--- Genus_Lab2/rtl/xnor_popcount/xnor_popcount.vhd
+++ Genus_Lab2/rtl/xnor_popcount/xnor_popcount.vhd
@@ -32,21 +32,15 @@ entity xnor_popcount is
 end xnor_popcount;
 
 architecture Behavioral of xnor_popcount is
-  constant bit_width : integer := 256;
-  constant levels : integer := N/bit_width + 1;
+  constant bit_width : integer := 16;
+  -- constant levels : integer := N/bit_width + 1;
+  constant levels : integer := 1;
 
   signal top_array : std_logic_vector(0 to N-1);
-  -- signal end_sum : integer range 0 to 768 := 0;
 
 
-  type propagate_array is array(0 to N/bit_width-1) of std_logic_vector(0 to N-1);
-  signal propagate : propagate_array;
-
-  type sums_array is array(0 to N/bit_width-1) of integer range 0 to bit_width;
-
-
-  type total_sums_array is array(0 to N/bit_width-1) of integer range 0 to 768;
-  signal total_sums : total_sums_array;
+  type in_between_array_type is array(0 to N/bit_width - 1) of integer range 0 to bit_width;
+  signal in_between_array : in_between_array_type;
 
 
 
@@ -59,7 +53,8 @@ begin
       variable will_be_valid : integer range 0 to levels := 0;
 
 
-      variable sums : sums_array;
+      variable in_between : integer range 0 to bit_width;
+      variable total_sum : integer range 0 to N;
 
       variable line_out : line;
 
@@ -73,61 +68,36 @@ begin
           else
             if enable = '1' then
 
-
-            -- first level
-            propagate(0) <= input_input xnor input_weights;
-
-
-            -- rest of the levels
-            for i in 1 to N/bit_width-1 loop
-              propagate(i)(i*bit_width to N-1) <= propagate(i-1)(i*bit_width to N-1);
-            end loop;
-
-            -- print all propagate values
-            for i in 0 to N/bit_width-1 loop
-              -- write(line_out, to_hstring(propagate(i)));
-
-
-              sums(i) := 0;
-              for j in i*bit_width to (i+1)*bit_width-1 loop
-                if propagate(i)(j) = '1' then
-                  sums(i) := sums(i) + 1;
+            -- level 0 to in between
+            for i in 0 to (N/bit_width - 1) loop
+              in_between := 0;
+              for j in 0 to (bit_width - 1) loop
+                if input_input(i*bit_width + j) = input_weights(i*bit_width + j) then
+                  in_between := in_between + 1;
                 end if;
               end loop;
-              -- write(line_out, string'(" "));
-              -- write(line_out, int_to_leading_zeros(sums(i), 2));
-
-
-              if i = 0 then
-                total_sums(i) <= sums(i);
-              else 
-                total_sums(i) <= total_sums(i-1) + sums(i);
-              end if;
-              -- write(line_out, string'(" "));
-              -- write(line_out, int_to_leading_zeros(total_sums(i), 3));
-
-              -- writeline(output, line_out);
+              in_between_array(i) <= in_between;
             end loop;
 
-            popcount_sum <= total_sums(N/bit_width-1);
 
-            if total_sums(N/bit_width-1) >= 384 then
+            -- in between to sum
+            total_sum := 0;
+            for i in 0 to (N/bit_width - 1) loop
+              total_sum := total_sum + in_between_array(i);
+            end loop;
+
+            popcount_sum <= total_sum;
+
+            if total_sum >= N/2 then
               is_sum_high <= '1';
             else
               is_sum_high <= '0';
             end if;
 
 
-            -- writeline(output, line_out);
-            -- writeline(output, line_out);
-            -- writeline(output, line_out);
-
-
-
 
             if will_be_valid = levels then
               is_valid <= '1';
-              will_be_valid := 0;
             else
               will_be_valid := will_be_valid + 1;
             end if;
